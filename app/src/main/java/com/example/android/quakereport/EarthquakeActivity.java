292d68766b515288639c6b24a48461ae2fae34fa
nsicilia <nsicilia@oswego.edu>
2019-01-21 20:38:40 -0500

    EarthquakeActivity: Added functionality to determine whether or not an internet connection is available and display and error message if no internet connection is found. AndroidManifest: added ACCESS_NETWORK_STATE permission.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,9 +16,12 @@
 package com.example.android.quakereport;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -68,12 +71,28 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         listView.setAdapter(mAdapter);
 
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
+          //Query the active network and determine if it has Internet connectivity.
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,EarthquakeActivity.this);
+        if (isConnected == true ){
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,EarthquakeActivity.this);
+        }else {
+            ProgressBar spinner = findViewById(R.id.loading_spinner);
+            //Sets the loading spinner to invisible
+            spinner.setVisibility(View.GONE);
+            //Sets view background to a light purple color
+            mEmptyStateTextView.setBackgroundColor(Color.parseColor("#ce93d8"));
+            // Set empty state text to display "No earthquakes found."
+            mEmptyStateTextView.setText(R.string.no_internet);
+        }
 
 
 
